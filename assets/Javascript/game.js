0555183a4593597460e0e25415b713adad85a1a0
Mag2017 <Magbor.Atem@gmail.com>
2017-06-18 15:24:37 -0400

    adding correct files

---
+++ assets/Javascript/game.js
@@ -0,0 +1,14 @@
+
+
+var Guesses = {
+        g1: ["a","b"."c","d","e","f"."g","h","i","j","k","l","m","n","o","p","q","r","s"."t","u","v","w","x","y","z"];
+        g2: ["a","b"."c","d","e","f"."g","h","i","j","k","l","m","n","o","p","q","r","s"."t","u","v","w","x","y","z"];
+        g3: ["a","b"."c","d","e","f"."g","h","i","j","k","l","m","n","o","p","q","r","s"."t","u","v","w","x","y","z"];
+        g4: ["a","b"."c","d","e","f"."g","h","i","j","k","l","m","n","o","p","q","r","s"."t","u","v","w","x","y","z"];
+       
+};
+
+var Wins = 0;
+var Loses =0;
+var Guesses So Far = 0;
+var GuessArray  = [];
