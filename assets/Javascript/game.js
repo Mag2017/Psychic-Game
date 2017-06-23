39a16f3aa06a59984f1614cbaf4f3d4bf71865d9
Mag2017 <Magbor.Atem@gmail.com>
2017-06-22 20:01:47 -0400

    My commit

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,4 +1,4 @@
-
+<script>
 
 var Guesses = {
         g1: ["a","b"."c","d","e","f"."g","h","i","j","k","l","m","n","o","p","q","r","s"."t","u","v","w","x","y","z"];
@@ -12,3 +12,6 @@ var Wins = 0;
 var Loses =0;
 var Guesses So Far = 0;
 var GuessArray  = [];
+
+
+</script>
